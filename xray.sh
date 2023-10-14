64637b871322222b2735c80dbb983af500fa6219
iamtrazy <iamtrazy@proton.me>
2023-10-14 22:06:32 +0530

    feat: added arm64 architecure support

--- xray.sh
+++ xray.sh
@@ -2,37 +2,22 @@
 
 mkdir -p /var/log/xray /var/log/caddy /usr/share/xray
 
-PLATFORM=$1
-if [ -z "$PLATFORM" ]; then
-    ARCH="amd64"
-else
-    case "$PLATFORM" in
-        linux/386)
-            ARCH="386"
+
+if [[ "$(uname)" == 'Linux' ]]; then
+    case "$(uname -m)" in
+        'amd64' | 'x86_64')
+            ARCH='amd64'
         ;;
-        linux/amd64)
-            ARCH="amd64"
-        ;;
-        linux/arm/v6)
-            ARCH="arm6"
-        ;;
-        linux/arm/v7)
-            ARCH="arm7"
-        ;;
-        linux/arm64|linux/arm64/v8)
-            ARCH="arm64"
-        ;;
-        linux/ppc64le)
-            ARCH="ppc64le"
-        ;;
-        linux/s390x)
-            ARCH="s390x"
+        'armv8' | 'aarch64')
+            ARCH='arm64'
         ;;
         *)
             ARCH=""
         ;;
+        
     esac
 fi
+
 [ -z "${ARCH}" ] && echo "Error: Not supported OS Architecture" && exit 1
 # Download binary file
 XRAY_FILE="xray_linux_${ARCH}"
